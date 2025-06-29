d4effd01b1aac8a61e7a5523906998a61b1c14bb
SoonHyung_LEE <tnsgud9@naver.com>
2025-06-29 16:56:38 +0900

    Add example

--- example/packet_capture.py
+++ example/packet_capture.py
@@ -1,9 +1,11 @@
 from scapy.all import *
 
+
 # 패킷 캡처 후 호출되는 콜백 함수 정의
 def packet_callback(packet):
-   # 캡처된 패킷의 요약(summary)을 출력합니다.
-   print(f"캡처된 패킷: {packet.summary()}")
+    # 캡처된 패킷의 요약(summary)을 출력합니다.
+    print(f"캡처된 패킷: {packet.summary()}")
+
 
 # sniff() 함수는 네트워크에서 패킷을 캡처하는 함수.
 # prn 인자는 패킷이 캡처될 때마다 호출될 콜백 함수.
