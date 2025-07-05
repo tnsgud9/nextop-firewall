1b7a1b52a0b0ac2fa8bae73ac77d7dcdc83633e3
SoonHyung_Lee <tnsgud9@naver.com>
2025-07-05 09:59:54 +0900

    Fix command

--- src/firewall/command.py
+++ src/firewall/command.py
@@ -1,18 +1,8 @@
-from dataclasses import dataclass
-from typing import Callable, Dict
-
-
-_command_handlers: Dict[str, Callable] = {}
-
-
-@dataclass
-class CommandResult:
-    action: str
-    data: str
+_command_handlers: dict[str, callable] = {}
 
 
 def command(name: str):
-    def decorator(func: Callable):
+    def decorator(func: callable):
         if _command_handlers.get(name):
             raise Exception(
                 f"The @command decorator should not be applied multiple times."
@@ -23,14 +13,14 @@ def command(name: str):
     return decorator
 
 
-def get_command_handler(cmd: str) -> tuple[Callable, str] | None:
+def get_command_handler(cmd: str) -> tuple[callable, str] | None:
     for prefix in _command_handlers:
         if cmd.startswith(prefix):
             return _command_handlers[prefix], cmd[len(prefix) :].strip()
     return None
 
 
-async def executeCommand(cmd: str, controller) -> CommandResult | None:
+async def executeCommand(cmd: str, controller):
     handler_entry = get_command_handler(cmd)
 
     if handler_entry is None:
