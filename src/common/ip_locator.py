528100a93cf1173d70ac587d7728ea81f259c111
SoonHyung_Lee <tnsgud9@naver.com>
2025-07-26 23:21:09 +0900

    change ip locator

--- src/common/ip_locator.py
+++ src/common/ip_locator.py
@@ -1,32 +1,14 @@
-# src/firewall/utils/ip_country_resolver.py
-
-import ipaddress
-import csv
 import os
+import geoip2.database
 
 
-# 실행 시 단 1회만 CSV 로드
-def _load_ip_ranges():
-    ip_ranges = []
-    csv_path = os.path.join(os.path.dirname(__file__), "ipv4.csv")
-    with open(csv_path, newline="", encoding="utf-8") as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            start_ip = ipaddress.IPv4Address(row["start_ip"])
-            end_ip = ipaddress.IPv4Address(row["end_ip"])
-            country_code = row["country_code"]
-            ip_ranges.append((start_ip, end_ip, country_code))
-    return ip_ranges
+db_path = os.path.join(os.path.dirname(__file__), "GeoLite2-Country.mmdb")
 
 
-# 전역 범위에 로드 (한 번만 실행됨)
-_ip_ranges = _load_ip_ranges()
-
-
-# 이 함수만 외부에 노출
-def find_country_code(ip_str):
-    ip = ipaddress.IPv4Address(ip_str)
-    for start_ip, end_ip, country_code in _ip_ranges:
-        if start_ip <= ip <= end_ip:
-            return country_code
-    return "N/A"
+def find_country_code(ip_address):
+    try:
+        with geoip2.database.Reader(db_path) as reader:
+            response = reader.country(ip_address)
+            return response.country.iso_code  # 국가 코드 (예: 'US', 'KR')
+    except:
+        return "N/A"
